7734b980bf92346e1fa97cd851d4b4763877654b
Roman Molotov <r.molotov@yahoo.com>
2024-03-08 23:42:58 +0300

    add identity and usermanagement

---
+++ Services/UserManagement/UserManagement.WebApi/Program.cs
@@ -0,0 +1,68 @@
+using IdentityServer4.AccessTokenValidation;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Logging;
+using UserManagement.Core.Entities;
+using UserManagement.Persistence.Database;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddDbContext<UsersDbContext>(optionsBuilder =>
+    {
+        optionsBuilder.UseSqlServer(builder.Configuration.GetConnectionString("UsersConnection"));
+    });
+
+builder.Services
+    .AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<UsersDbContext>()
+    .AddDefaultTokenProviders();
+builder.Services
+    .AddAuthentication(IdentityServerAuthenticationDefaults.AuthenticationScheme)
+    .AddIdentityServerAuthentication(options =>
+    {
+        options.Authority = builder.Configuration.GetValue<string>("IdentityUri");
+        options.RequireHttpsMetadata = false;
+    });
+builder.Services
+    .AddAuthorizationBuilder()
+    .AddPolicy("ApiScopePolicy", policy =>
+    {
+        policy.RequireAuthenticatedUser();
+        policy.RequireClaim("scope", "ECR.Api");
+    });
+
+builder.Services
+    .AddEndpointsApiExplorer()
+    .AddSwaggerGen()
+    .AddControllers();
+
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<UsersDbContext>();
+    if (context.Database.GetPendingMigrations().Any())
+        context.Database.Migrate();
+}
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+IdentityModelEventSource.ShowPII = true;
+
+app
+    .UseHttpsRedirection()
+    .UseRouting()
+    .UseAuthentication()
+    .UseAuthorization();
+
+app
+    .MapControllers()
+    .RequireAuthorization("ApiScopePolicy");
+
+app.Run();
