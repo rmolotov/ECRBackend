c238c438bff232ea3347c7c60c6fd81a9c26f8fe
Roman Molotov <r.molotov@yahoo.com>
2024-06-02 20:31:01 +0300

    make get enemies query cacheable

--- Services/RemoteConfig/RemoteConfig.Application/Enemies/Queries/GetEnemiesListQuery.cs
+++ Services/RemoteConfig/RemoteConfig.Application/Enemies/Queries/GetEnemiesListQuery.cs
@@ -1,9 +1,10 @@
-using MediatR;
 using RemoteConfig.Application.Enemies.Responses;
+using RemoteConfig.Application.Interfaces;
 
 namespace RemoteConfig.Application.Enemies.Queries;
 
-public class GetEnemiesListQuery : IRequest<IList<GetEnemyResponse>>
+public class GetEnemiesListQuery : ICacheableRequest<IList<GetEnemyResponse>>
 {
-    
+    public string CacheKey { get; init; } = nameof(GetEnemiesListQuery);
+    public DateTime? ExpiredAt { get; init; }
 }
